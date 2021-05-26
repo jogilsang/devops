8d8e749a7cd60e9b4c5ee345fe93b19a14acab69
GilSang Jo <jogilsang@gmail.com>
2021-05-27 03:10:02 +0900

    feature : 5.postman - tests - save_json.js

---
+++ 5.postman/pre_request_scripts/save_json.js
@@ -0,0 +1,10 @@
+// title : JSON결과 저장하기
+// contents : Postman - tests Scripts
+// dated : 2021-05-27
+// created : jogilsang
+
+const responseJson = pm.response.json();
+var access_token = responseJson.data.access_token;
+
+console.log(access_token);
+pm.collectionVariables.set('access_token', access_token);
